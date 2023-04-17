d1b0993acf4b3f50c758b63536e0feadc5decae7
Radhika <radhika.b4329@gmail.com>
2023-04-17 23:15:19 +0530

    loops

--- 08-loops/count/main.tf
+++ 08-loops/count/main.tf
@@ -26,7 +26,7 @@ variable "components" {
       instance_type = "t3.small"
     }
     catalogue = {
-      name = "catalogue",
+      name = "catalogue"
       instance_type = "t3.micro"
     }
   }
